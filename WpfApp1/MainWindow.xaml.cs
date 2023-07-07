81c23639dafc1d797e53f570bcff75290c9ececc
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-07-07 10:30:58 +0400

    70%

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Photo> pictureList = new ObservableCollection<Photo>() 
+        {
+            new Photo("C:\\Users\\Elgun\\source\\repos\\WpfApp1\\WpfApp1\\Pictures\\child-2715429_640.webp"),
+            new Photo("C:\\Users\\Elgun\\source\\repos\\WpfApp1\\WpfApp1\\Pictures\\elephant-1598359_640.webp" ),
+            new Photo("C:\\Users\\Elgun\\source\\repos\\WpfApp1\\WpfApp1\\Pictures\\rat-152162_640.webp"),
+            new Photo("C:\\Users\\Elgun\\source\\repos\\WpfApp1\\WpfApp1\\Pictures\\th.jpg")
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            Pictures.ItemsSource= pictureList;
+            DataContext = this;
+        }
+
+        private void Image_Click(object sender, RoutedEventArgs e)
+        {
+            Button Image = sender as Button;
+            Image image = Image.Content as Image;
+            BitmapImage bitmapImage = new BitmapImage(new Uri(image.Source.ToString(), UriKind.RelativeOrAbsolute));
+            
+            Gallery gallery = new Gallery();
+            Image gImage = (Image)gallery.FindName("Current");
+            gImage.Source= bitmapImage;
+            gallery.ShowDialog();
+        }
+    }
+}
---
+++ WpfApp1/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Photo
+    {
+        public Photo(string? image)
+        {
+            Image = image;
+        }
+
+        public string? Image { get; set; }
+    }
+}
